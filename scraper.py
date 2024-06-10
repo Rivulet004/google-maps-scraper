dc849f948bdcf8742380fc285ea8f6970c51aa2c
Muhammad Bin Shafique <mbinshafique29@gmail.com>
2024-06-11 00:42:14 +0500

    updated collect_listing_data() to handle the click via JavaScript, as the default click was skiping entries and this resulted in duplicates, handled click intecepted exception

--- scraper.py
+++ scraper.py
@@ -1,8 +1,11 @@
-import sys, pyperclip
+import sys, pyperclip, os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    ElementClickInterceptedException,
+)
 import time
 import csv
 
@@ -22,19 +25,24 @@ Reset = "\033[0m"
 class ScrapGoogleMap:
     def __init__(self, query, location, type):
         self.query = query
+        self.location = location
+        self.type = type
         self.driver = webdriver.Firefox()
         self.all_listings = []
         self.list_info = []
         self.location_data = {}
+        self.counter = 0
 
     def open_google_maps(self):
         url = "https://www.google.com/maps/search/"
+
+        # Create a searchable url by the browser
         split = self.query.split()
-        url = url + split[0]
+        url = url + split[0]  # initialize the url to avoid the + sign in the start
         for i in range(1, len(split)):
             url += f"+{split[i]}"
 
-        print(Red + f"Opening Google Maps for query: {self.query}" + Reset)
+        print(Yellow + f"Opening Google Maps for query: {self.query}" + Reset)
         self.driver.get(url)
         print(Green + "Google Maps opened" + Reset)
 
@@ -43,7 +51,7 @@ class ScrapGoogleMap:
             By.CSS_SELECTOR, f"div[aria-label='Results for {self.query}']"
         )
         keepScrolling = True
-        print(Yellow + "Scrolling through listings to load all results" + Reset)
+        print(Yellow + "Scrolling through listings to load all results..." + Reset)
         while keepScrolling:
             div_sidebar.send_keys(Keys.PAGE_DOWN)
             time.sleep(0.5)
@@ -61,6 +69,7 @@ class ScrapGoogleMap:
         self.all_listings = self.driver.find_elements(By.CLASS_NAME, "hfpxzc")
         if self.all_listings:
             print(f"{len(self.all_listings)} listings retrieved")
+            self.counter += len(self.all_listings)
         else:
             print(Yellow + "No listings found" + Reset)
 
@@ -71,10 +80,26 @@ class ScrapGoogleMap:
             return
         for listing in self.all_listings:
             print(Yellow + f"Collecting data from listing {i}..." + Reset)
-            listing.click()
 
-            # add time for the lisitng to load
-            time.sleep(1)
+            # Using JS to clink the element
+            try:
+                self.driver.execute_script(
+                    "arguments[0].scrollIntoView(true);", listing
+                )
+                time.sleep(0.3)
+                self.driver.execute_script("arguments[0].click();", listing)
+
+                # add time for the lisitng to load
+                time.sleep(0.3)
+
+            except ElementClickInterceptedException:
+                print(
+                    Red
+                    + f"Listing {i} is not clickable. Skipping to the next listing."
+                    + Reset
+                )
+                i += 1
+                continue
 
             self.location_data = {
                 "name": "NA",
@@ -97,6 +122,7 @@ class ScrapGoogleMap:
                 listing_url = self.driver.current_url
             except NoSuchElementException:
                 listing_url = None
+
             try:
                 type = self.driver.find_element(By.CLASS_NAME, "DkEaL")
             except NoSuchElementException:
@@ -161,6 +187,9 @@ class ScrapGoogleMap:
             self.list_info.append(self.location_data)
             print(Green + f"Collected data from listing {i}:" + Reset)
             print(Blue + f"{self.location_data}" + Reset)
+
+            # if i == 2:
+            #     break
             i += 1
 
     def save_data_to_csv(self):
@@ -169,7 +198,10 @@ class ScrapGoogleMap:
             print(Red + "No data to write to CSV." + Reset)
             return
         with open(
-            f"data/{self.query}.csv", mode="w", newline="", encoding="utf-8"
+            f"data/{self.location}/{self.query}.csv",
+            mode="w",
+            newline="",
+            encoding="utf-8",
         ) as file:
             writer = csv.DictWriter(file, fieldnames=self.list_info[0].keys())
 
@@ -191,27 +223,44 @@ def main():
     else:
         location = pyperclip.paste()
 
+    # Check if the directory exists to save file if not then create the directory
+    if not os.path.exists(f"data/{location}"):
+        os.makedirs(f"data/{location}")
+
     with open("business_list.txt", "r") as file:
         business_list = file.readlines()
 
     cleaned_business_list = [entry.strip() for entry in business_list]
+    total_counter = 0
 
     for type in cleaned_business_list:
 
         query = f"{type} in {location}"
+        print(
+            Cyan
+            + "<――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――>"
+            + Reset
+        )
+        print(Magenta + f"Initializing Scraper for the query: {query}" + Reset)
+        scraper = ScrapGoogleMap(query, location, type)
+        print(Green + f"Scraper initialized" + Reset)
+        scraper.open_google_maps()
+        scraper.scroll_to_load_all_listings()
+        scraper.retrieve_listings()
+        total_counter += scraper.counter
+        scraper.collect_listing_data()
+        scraper.save_data_to_csv()
+        scraper.close_browser()
+        print(Green + f"Data Scraped for the query: {query}" + Reset)
+        print(Red + f"total entries as of yet {total_counter}" + Reset)
+        print(
+            Cyan
+            + "<――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――>"
+            + Reset
+        )
 
-        print(f"{query} | {type} | {location}")
-        time.sleep(0.1)
-        # scraper = ScrapGoogleMap(query, location, type)
-        # scraper.open_google_maps()
-        # scraper.scroll_to_load_all_listings()
-        # scraper.retrieve_listings()
-        # scraper.collect_listing_data()
-        # scraper.save_data_to_csv()
-        # scraper.close_browser()
     print(Yellow + "Terminating Program" + Reset)
     print(Red + "Program Terminated Sucessfully" + Reset)
-
 
 
 if __name__ == "__main__":
